5e22890f1c4aadf705de45b3fc6f9cb30526715c
Hadrien Meyer <57682091+Andrew808s@users.noreply.github.com>
2019-12-18 13:14:58 +0100

    Update Transform.py

--- Transformation_Example/Transform.py
+++ Transformation_Example/Transform.py
@@ -1,11 +1,8 @@
 ######################################################
 import numpy as np
 import pandas as pd
-from skimage import data
 from skimage import transform as tf
 ######################################################
-
-
 
 
 class Transformation:
@@ -15,42 +12,54 @@ class Transformation:
 		""" Input & Output are .txt files
 			Input contains Landmarks (named Repere) and points to tranfsorm (named Mesures)
 			Output only contains Landmarks in the new system """
+		#Load the textfile into the class
 		self.input_ = Input
 		self.output_ = Output
+
+		#Create Dataframes from the textfiles
 		self.Input_ = pd.read_csv(Input)
 		self.Output_ = pd.read_csv(Output)
-		self.Repere_init_ = self.Input_['Type'] == 'Repere'
-		self.Repere_init_array_ = np.zeros([self.Input_[self.Repere_init_].shape[0],2])
-		self.Repere_init_array_[:,0] = self.Input_[self.Repere_init_]['X']
-		self.Repere_init_array_[:,1] = self.Input_[self.Repere_init_]['Y']
-		self.Repere_final_ = self.Output_['Type'] == 'Repere'
-		self.Repere_final_array_ = np.zeros([self.Output_[self.Repere_final_].shape[0],2])
-		self.Repere_final_array_[:,0] = self.Output_[self.Repere_final_]['X']
-		self.Repere_final_array_[:,1] = self.Output_[self.Repere_final_]['Y']
-		self.Mesures_init_ = self.Input_['Type'] == 'Mesure'
-		self.Mesures_init_array_ = np.zeros([self.Input_[self.Mesures_init_].shape[0],2])
-		self.Mesures_init_array_[:,0] = self.Input_[self.Mesures_init_]['X']
-		self.Mesures_init_array_[:,1] = self.Input_[self.Mesures_init_]['Y']
+		#Extract Landmarks from initial system
+		self.Repere_init_ = self.Input_[self.Input_['Type'].str.contains('Repere')]
+		#Create array of initial landmark values for transform calculations
+		self.Repere_init_array_ = np.zeros([self.Repere_init_.shape[0],2])
+		#Fill the array with the values
+		self.Repere_init_array_[:,0] = self.Repere_init_['X']
+		self.Repere_init_array_[:,1] = self.Repere_init_['Y']
+		#Extract Landmarks from final system
+		self.Repere_final_ = self.Output_[self.Output_['Type'].str.contains('Repere')]
+		#Create array of final landmark values for transform calculations
+		self.Repere_final_array_ = np.zeros([self.Repere_final_.shape[0],2])
+		#Fill the array with the values
+		self.Repere_final_array_[:,0] = self.Repere_final_['X']
+		self.Repere_final_array_[:,1] = self.Repere_final_['Y']
+		#Extract measurements values from initial system
+		self.Mesures_init_ = self.Input_[self.Input_['Type'].str.contains('Mesure')]
+		#Create array of initial measurements for transform calculations
+		self.Mesures_init_array_ = np.zeros([self.Mesures_init_.shape[0],2])
+		#Fill the array with the values
+		self.Mesures_init_array_[:,0] = self.Mesures_init_['X']
+		self.Mesures_init_array_[:,1] = self.Mesures_init_['Y']
 
 
 	def get_repere_init(self):
 		""" Return a dataframe of the input landmarks"""
-		return self.Input_[self.Repere_init_]
+		return self.Input_[self.Input_['Type'].str.contains('Repere')]
 
 	def get_repere_final(self):
 		""" Return a dataframe of the output landmarks """
-		return self.Output_[self.Repere_final_]
+		return self.Output_[self.Output_['Type'].str.contains('Repere')]
 
 	def get_mesures_init(self):
 		""" Return a dataframe of the points to transform from the input """
-		return self.Input_[self.Mesures_init_]
+		return self.Input_[self.Input_['Type'].str.contains('Mesure')]
 
 	def transform(self,type):
 		""" In our working cases the transformation should only be affine (Rotation,Translation and Shear).
 			But in general cases, the transformation could also have deformation and the transformation is 
 			a projection. 
 			See Mathematical definition of affine transformation and Projection (Homography) for more details. """
-		if type == 'Affine :
+		if type == 'Affine' :
 			self.transformation_ = tf.AffineTransform()
 			self.transformation_.estimate(self.Repere_init_array_, self.Repere_final_array_)
 			self.Mesures_final_array_ = self.transformation_(self.Mesures_init_array_)
@@ -74,10 +83,8 @@ class Transformation:
 		""" Write the calculated coordinates into the output textfile """
 		fd = open(self.output_,"a")
 		for i in range(self.Mesures_final_array_.shape[0]):
-	 		fd.write('{},{},{}\n'.format('Mesures',self.Mesures_final_array_[i,0],self.Mesures_final_array_[i,1]))
+	 		fd.write('{},{},{}\n'.format('Mesures'+str(i+1),self.Mesures_final_array_[i,0],self.Mesures_final_array_[i,1]))
 		fd.close()
-
-
 
 
 ################################################################################################################################
